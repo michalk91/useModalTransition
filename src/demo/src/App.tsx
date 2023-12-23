be93f49703a0731b5851b638a24c867664691a89
Michal Kukla <michal.kukla91@gmail.com>
2023-12-23 21:43:32 +0100

    Routing added to App and main

--- src/demo/src/App.tsx
+++ src/demo/src/App.tsx
@@ -1,35 +1,72 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Routes, Route, Link } from "react-router-dom";
+import Gallery from "./examples/Gallery";
+import ContactBtn from "./examples/ContactBtn";
+import Squares from "./examples/Squares";
+import { CSSProperties } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const liStyle: CSSProperties = {
+    fontSize: "20px",
+    textAlign: "center",
+    listStyleType: "none",
+    padding: "0 20px",
+  };
+
+  const LinkStyle: CSSProperties = {
+    textDecoration: "none",
+  };
+
+  const textCenter: CSSProperties = {
+    textAlign: "center",
+  };
+
+  const navStyle: CSSProperties = {
+    backgroundColor: "lightgray",
+    padding: "10px",
+  };
+
+  const ulStyle: CSSProperties = {
+    display: "flex",
+    flexWrap: "wrap",
+    justifyContent: "center",
+  };
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+        <h1 style={textCenter}>useModalTransition</h1>
+
+        <p style={textCenter}>
+          The examples below illustrate some of the features of the Modal
+          Transition React hook
         </p>
+        <nav style={navStyle}>
+          <ul style={ulStyle}>
+            <li style={liStyle}>
+              <Link style={LinkStyle} to="/squares">
+                Squares
+              </Link>
+            </li>
+            <li style={liStyle}>
+              <Link style={LinkStyle} to="/gallery">
+                Gallery
+              </Link>
+            </li>
+            <li style={liStyle}>
+              <Link style={LinkStyle} to="/contactbtn">
+                Contact Button
+              </Link>
+            </li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route path="/squares" element={<Squares />}></Route>
+          <Route path="/gallery" element={<Gallery />}></Route>
+          <Route path="/contactbtn" element={<ContactBtn />}></Route>
+        </Routes>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
